690249352d4b2a4138f1e04f6a509c6a7f8dec57
sebasryes <83566772+sebasryes@users.noreply.github.com>
2022-03-08 16:46:44 -0600

    Update memoriaRam.py

--- memoriaRam.py
+++ memoriaRam.py
@@ -4,12 +4,10 @@
 # Mario Puente 21290
 # Sebastián Reyes 21139
 
-#imports
 import simpy
 import random
 import statistics
 
-#Se declaran variables
 capRAM = 100
 interval = 10
 res = []
@@ -24,11 +22,11 @@ def new(env, nom, capRAM, res):
     #Comienza a correr el tiempo al crear el proceso
     inicio = env.now
     memoria = random.randint(1,10)
-    print('Proceso %s creado en %d' %(nom, inicio))
+    print(' El proceso %s creado en %d' %(nom, inicio))
 
     #Se revisa que hay memoria disponible
     if memoria > capRAM.capacity:
-        print("Proceso %s espera a recibir memoria RAM en %d" %(nom, env.now))
+        print("El proceso %s esta en espera de recibir memoria de la RAM %d" %(nom, env.now))
     else:
         capRAM.get(memoria)
         yield env.process(ready(env, nom, capRAM, memoria, inicio))
@@ -36,29 +34,29 @@ def new(env, nom, capRAM, res):
 
 def ready(env, nom, capRAM, memoria, inicio):
     #El proceso es atendido por el CPU
-    print("Proceso %s espera a ser atendido por el CPU  en %d" %(nom, env.now))
+    print("El proceso %s esta en espera del CPU %d" %(nom, env.now))
     yield env.process(running(env, nom, capRAM, res, memoria, inicio))
 
 def running(env, nom, capRAM, res, memoria, inicio):
     terminated = False
     instrucciones = random.randint(1,10)
     destino = random.randint(1,2)
-    #Se hace el proceso hasta que no queden instrucciones
+   
     while terminated == False:
         instrucciones = instrucciones - inst
         yield env.timeout(1)
 
         if instrucciones > 2:
             if destino == 1:
-                print("Proceso %s hace operacion I/O en %d" % (nom, env.now))
+                print("El proceso %s hace operacion I/O en %d" % (nom, env.now))
                 yield env.timeout(1)
 
         else:
             terminated = True
 
-    #Se termina el proceso
+   
 
-    print("Proceso %s ha terminado en %d" %(nom, env.now))    
+    print("El proceso %s ha terminado en %d" %(nom, env.now))    
     capRAM.put(memoria)
     end = env.now
     tiempo = end - inicio
@@ -70,14 +68,14 @@ def ej(env, procesos, interval, capRAM, res):
         yield env.timeout(t)
         env.process(new(env, i, capRAM, res))
 
-#Se crea el ambiente, contenedor y recurso y luego se ejecuta el programa
+
 env = simpy.Environment()
 procesadores = simpy.Resource(env, capacity=proc)
 ram = simpy.Container(env, init=capRAM, capacity=capRAM)
 env.process(ej(env, procesos, interval, ram, res))
 env.run()
 
-#Se calcula el promedio y desviacion estandar
+#promedio y desviacion estandar
 r = 0
 for i in res:
     r = r + i
